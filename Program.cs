6e793bb5f6d65f5d41d68f169c3f2b82e767eed0
Yogesh <yogeshbietjhs@gmail.com>
2022-06-19 11:57:58 +0530

    head and tail

--- Program.cs
+++ Program.cs
@@ -4,8 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Assingment day 5");
-        }
+            int head = 0;
+            int tail = 0;
+            int numOfTime = 5;
+            int[] coin = new int[numOfTime];
 
+            int i = 0;
+            while (i < coin.Length)
+            {
+                Random random = new Random();
+                int Flip = random.Next(2);
+
+                if (Flip < 0.5)
+                {
+                    tail++;
+                    Console.WriteLine("No of head:"+ tail);
+                    i++;
+                }
+                else
+                {
+                    head++;
+                    Console.WriteLine("No of tail:" + head);
+                    i++;
+                }
+            }
+
+
+            int tailPercentage = (tail*100/numOfTime);
+            Console.WriteLine("Percentage of head:" + tailPercentage);
+            int HeadPercentage = (head*100/numOfTime);
+            Console.WriteLine("Percentage of tail:" + HeadPercentage);
+
+
+
+
+
+
+
+
+
+
+        }
     }
 }
