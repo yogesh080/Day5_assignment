a849da4ed5958d8abe41a882018830058bb94caa
Yogesh <yogeshbietjhs@gmail.com>
2022-06-19 16:33:02 +0530

    QuotientandRemainder

--- Program.cs
+++ Program.cs
@@ -4,33 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a (int) Number: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            int a,b;
-            int flag = 0;
-            int count = 0;
+            Console.WriteLine("Enter the dividend");
+            int dividend = int.Parse(Console.ReadLine());
 
-            for (a = 2; a <= num; a++)
-            {
-                if(num % a == 0)
-                {
-                     count = 0;
-                     for(b = 1; b <= a; b++)
-                    {
-                        if (a%b == 0)
-                            count++;    
-                    }
-                    if(count == 2)
-                    {
-                        flag = 1;
-                        Console.WriteLine("prime factor of number is :  " + a);
-                    }
-                }
+            Console.WriteLine("Enter the divisor");
+            int divisor = int.Parse(Console.ReadLine());
 
-            }
-            if(flag == 0)
-                Console.WriteLine("there is no Prime factor");
-            
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Quotient is: " + quotient);
+            Console.WriteLine("Remainder is: " + remainder);
+
+
+
+
+
         }
     }
 }
