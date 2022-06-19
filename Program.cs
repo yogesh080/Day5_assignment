c012411dffa8e78372d9642c5dc9a2af426a8e64
yogesh <93078938+yogesh080@users.noreply.github.com>
2022-06-19 16:58:04 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            int a = 5, b = 10;
-            Console.WriteLine("Before swap a= "+a+" b= "+b);
-            a=a*b; //a=50 (5*10)      
-            b=a/b; //b=5 (50/10)      
-            a=a/b; //a=10 (50/5)    
-            Console.Write("After swap a= "+a+" b= "+b);
+            int num1 = 5, num2 = 10;
+            Console.WriteLine("Before swap a= "+num1+" b= "+num2);
+            num1=num1*num2;      
+            num2=num1/num2;      
+            num1=num1/num2;    
+            Console.Write("After swap a= "+num1+" b= "+num2);
 
 
         }
