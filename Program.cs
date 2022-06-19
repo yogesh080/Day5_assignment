1258bf0c75127a7637212f01308bee8baa153b86
Yogesh <yogeshbietjhs@gmail.com>
2022-06-19 12:15:51 +0530

    leap year

--- Program.cs
+++ Program.cs
@@ -4,46 +4,17 @@
     {
         static void Main(string[] args)
         {
-            int head = 0;
-            int tail = 0;
-            int numOfTime = 5;
-            int[] coin = new int[numOfTime];
+            Console.WriteLine("Enter a number for leap year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
 
-            int i = 0;
-            while (i < coin.Length)
+            if (year%4==0)
             {
-                Random random = new Random();
-                int Flip = random.Next(2);
-
-                if (Flip < 0.5)
-                {
-                    tail++;
-                    Console.WriteLine("No of head:"+ tail);
-                    i++;
-                }
-                else
-                {
-                    head++;
-                    Console.WriteLine("No of tail:" + head);
-                    i++;
-                }
+                Console.WriteLine("Is leap year");
             }
-
-
-            int tailPercentage = (tail*100/numOfTime);
-            Console.WriteLine("Percentage of head:" + tailPercentage);
-            int HeadPercentage = (head*100/numOfTime);
-            Console.WriteLine("Percentage of tail:" + HeadPercentage);
-
-
-
-
-
-
-
-
-
-
+            else
+            {
+                Console.WriteLine("Is not leap year");
+            }
         }
     }
 }
